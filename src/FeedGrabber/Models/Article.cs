d78d73dba16c02b61c5912cd0021513c7616bd53
Hendy Racher <git@hendyracher.co.uk>
2016-09-03 18:58:22 +0100

    NPoco attributes applied to models

--- src/FeedGrabber/Models/Article.cs
+++ src/FeedGrabber/Models/Article.cs
@@ -1,19 +1,27 @@
 ﻿namespace FeedGrabber.Models
 {
     using System;
+    using NPoco;
 
+    [TableName("Article")]
     public class Article
     {
+        [Column("ArticleGUID")]
         public Guid ArticleGUID { get; set; }
 
+        [Column("FeedGUID")]
         public Guid FeedGUID { get; set; }
 
+        [Column("ArticleUrl")]
         public string ArticleUrl { get; set; }
 
+        [Column("ArticleTitle")]
         public string ArticleTitle { get; set; }
 
+        [Column("ArticleDescription")]
         public string ArticleDescription { get; set; }
 
+        [Column("PublishedDateTime")]
         public DateTime PublishedDateTime { get; set; }
     }
 }
--- src/FeedGrabber/Models/Feed.cs
+++ src/FeedGrabber/Models/Feed.cs
@@ -1,17 +1,24 @@
 ﻿namespace FeedGrabber.Models
 {
     using System;
+    using NPoco;
 
+    [TableName("Feed")]
     public class Feed
     {
+        [Column("FeedGUID")]
         public Guid FeedGuid { get; set; }
 
+        [Column("FeedUrl")]
         public string FeedUrl { get; set; }
 
+        [Column("FeedTitle")]
         public string FeedTitle { get; set; }
 
+        [Column("FeedDescription")]
         public string FeedDescription { get; set; }
 
+        [Column("QueryCount")]
         /// <summary>
         /// Number of times this feed has been queried
         /// </summary>
@@ -20,6 +27,7 @@
         /// <summary>
         /// The date time of this feed was last queried
         /// </summary>
+        [Column("LastQueryDateTime")]
         public DateTime LastQueryDateTime { get; set; }
     }
 }
