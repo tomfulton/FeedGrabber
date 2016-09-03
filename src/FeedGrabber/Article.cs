3992346251c6dfbccb4cc37c42dcd9ad24d3a85d
Tom Fulton <tomnf1@gmail.com>
2016-09-03 06:59:16 -0700

    Add quick downloader

---
+++ src/FeedGrabber/Article.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml;
+
+namespace FeedGrabber
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string Url { get; set; }
+
+        public string SourceName { get; set; }
+
+        public Article(XmlNode node)
+        {
+            var titleNode = node.SelectSingleNode("title");
+            if (titleNode != null)
+                Title = titleNode.Value;
+
+            var linkNode = node.SelectSingleNode("link");
+            if (linkNode != null)
+                Url = linkNode.Value;
+
+            var descriptionNode = node.SelectSingleNode("description");
+            if (descriptionNode != null)
+                Description = descriptionNode.Value;
+        }
+    }
+}
---
+++ src/FeedGrabber/FeedsDownloader.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Xml;
+
+namespace FeedGrabber
+{
+    public class FeedsDownloader
+    {
+        private List<Feed> _feeds;
+
+        public FeedsDownloader(List<Feed> feeds)
+        {
+            _feeds = feeds;
+        }
+
+        public List<Article> Download()
+        {
+            var allArticles = new List<Article>();
+
+            foreach (var feed in _feeds)
+            {
+                var articlesFromFeed = DownloadFromFeed(feed);
+                allArticles.AddRange(articlesFromFeed);
+            }
+
+            return allArticles;
+        }
+
+        private List<Article> DownloadFromFeed(Feed feed)
+        {
+            var request = WebRequest.Create(feed.Url);
+            var response = request.GetResponse();
+
+            var stream = response.GetResponseStream();
+
+            var rssDoc = new XmlDocument();
+            rssDoc.Load(stream);
+
+            var rssItems = rssDoc.SelectNodes("rss/channel/item");
+
+            var result = new List<Article>();
+            foreach (XmlNode rssItem in rssItems)
+            {
+                result.Add(new Article(rssItem));
+            }
+            return result;
+        }
+
+    }
+}
