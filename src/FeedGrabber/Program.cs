638fec5bdf7d6c33c9ae60d6fbb678a184fef057
Tom Fulton <tomnf1@gmail.com>
2016-09-03 06:41:42 -0700

    Initial commit

---
+++ src/FeedGrabber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedGrabber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private List<string> GetFeedUrls()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
